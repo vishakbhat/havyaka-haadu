ee4d77f133c51cfdcb254293521eee52d580555a
vishakbhat <43088128+vishakbhat@users.noreply.github.com>
2023-06-15 17:39:37 +1000

    update 15Jun2023

---
+++ icons/havyaka-fonts 2/havyaka_icons.dart
@@ -0,0 +1,23 @@
+// Place fonts/havyaka.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: havyaka
+//      fonts:
+//       - asset: fonts/havyaka.ttf
+import 'package:flutter/widgets.dart';
+
+class Havyaka {
+  Havyaka._();
+
+  static const String _fontFamily = 'havyaka';
+
+  static const IconData babygirl = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData Ganesha = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData Kalasha = IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData Namaste = IconData(0xe907, fontFamily: _fontFamily);
+  static const IconData rama = IconData(0xe908, fontFamily: _fontFamily);
+  static const IconData Swastik = IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData upnayan = IconData(0xe90a, fontFamily: _fontFamily);
+  static const IconData Wedding = IconData(0xe90b, fontFamily: _fontFamily);
+}
